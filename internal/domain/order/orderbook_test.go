17ebc0fa4062b45b06eb0cf79484acdee089d5f6
theghostmac <theghostmac@gmail.com>
2023-06-25 01:36:06 +0100

    test for orderbook passes

--- internal/domain/order/orderbook_test.go
+++ internal/domain/order/orderbook_test.go
@@ -1,6 +1,7 @@
 package order_test
 
 import (
+	"fmt"
 	"github.com/theghostmac/cryptex/internal/domain/order"
 	"testing"
 )
@@ -9,6 +10,10 @@ func TestNewLimit(t *testing.T) {
 	// Arrange.
 	testLimit := order.NewLimit(10_000)
 	buyOrder := order.NewOrder(true, 5)
+
+	testLimit.AddOrder(buyOrder)
+
+	fmt.Println(testLimit)
 }
 
 func TestNewOrder(t *testing.T) {
